9e95df6a87265d9a2bfae9dd1410f9e4ce018261
dewijones1975 <44159634+dewijones1975@users.noreply.github.com>
2019-02-25 10:45:51 +0000

    added echo
    Started echo class

--- app/src/main/java/teammerlin/mobilemerlin/Echo.java
+++ app/src/main/java/teammerlin/mobilemerlin/Echo.java
@@ -3,19 +3,49 @@ package teammerlin.mobilemerlin;
 import teammerlin.game.GameState;
 
 public class Echo extends Minigame {
+    private enum State {
+        UserInput(),
+        ChooseLength(),
+        CheckWinner(),
+        Playback();
+    }
 
+    private State state;
+    private int length;
     public Echo()
     {
         super();
+        state = State.ChooseLength;
+        length = 1;
     }
 
     @Override
     public void update(Panel panel)
     {
-        if(panel.getButton(1))
-        {
-            panel.setLight(1, 1);
+        int button = gridButtons(panel);
+
+        if(state==State.ChooseLength){
+            if(button!= -1) { //if button pressed
+                length = button;
+                panel.playSound("tttCross");
+                panel.clearLights();
+                panel.setLight(10,2);
+                state=State.UserInput;
+            }
         }
+        
+    }
+
+    private int gridButtons(Panel panel)
+    {
+        for(int i = 1; i < 10; i++)
+        {
+            if(panel.getButton(i))
+            {
+                return i;
+            }
+        }
+        return -1;
     }
 
 }
