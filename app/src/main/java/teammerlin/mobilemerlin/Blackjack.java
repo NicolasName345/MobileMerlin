08c73db916fa0ea552152b89dc7f5f3ea6db2160
Nicolas <nicolasname345@gmail.com>
2019-02-25 09:15:38 +0000

    Update Blackjack
    Fixed a loop error causing the game to crash on selection

--- app/src/main/java/teammerlin/mobilemerlin/Blackjack.java
+++ app/src/main/java/teammerlin/mobilemerlin/Blackjack.java
@@ -170,9 +170,9 @@ public class Blackjack extends Minigame {
         deck.add(9);
         deck.add(10);*/
 
-        for(i=0; i <=10; i++)
+        for(int j=0; j<=10; j++)
         {
-            deck.add(i);
+            deck.add(j);
         }
 
         Collections.shuffle(deck);
