434a1d56172aa1687814c1db4dd4eea081b0aa41
Dewi <dewijones1975@gmail.com>
2019-03-04 02:48:21 +0000

    Update Echo.java
    Added random note generator

--- app/src/main/java/teammerlin/mobilemerlin/Echo.java
+++ app/src/main/java/teammerlin/mobilemerlin/Echo.java
@@ -1,6 +1,13 @@
 package teammerlin.mobilemerlin;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+
 import teammerlin.game.GameState;
+import teammerlin.mobilemerlin.Minigame;
+import teammerlin.mobilemerlin.Panel;
 
 public class Echo extends Minigame {
     private enum State {
@@ -12,11 +19,30 @@ public class Echo extends Minigame {
 
     private State state;
     private int length;
+    private LinkedList<String> memory;
+    private int playbackPlace;
+    private int addNote;
+    private Random random = new Random();
+
+    ArrayList<String> list = new ArrayList<String>();
+
     public Echo()
     {
         super();
         state = State.ChooseLength;
         length = 1;
+        memory = new LinkedList<String>();
+        playbackPlace = 0;
+        addNote = 1;
+        list.add("mm0");
+        list.add("mm1");
+        list.add("mm2");
+        list.add("mm3");
+        list.add("mm4");
+        list.add("mm5");
+        list.add("mm6");
+        list.add("mm7");
+        list.add("mm8");
     }
 
     @Override
@@ -30,10 +56,46 @@ public class Echo extends Minigame {
                 panel.playSound("tttCross");
                 panel.clearLights();
                 panel.setLight(10,2);
+                //for(addNote=0; addNote<=length; addNote++) //Create random tune
+                //    memory.add(getRandomButton(list));
+                memory.add("mm5");
+                memory.add("mm6");
+                System.out.println(memory.size());
                 state=State.UserInput;
             }
         }
-        
+
+        if(state==State.UserInput){
+            //If Comp Turn is pressed
+            if(panel.getButton(14))
+            {
+                panel.setLight(10, 0);//Turn off light 10
+                state = State.Playback;
+            }
+        }
+
+        if(state==State.Playback){
+            if(playbackPlace != memory.size())
+            {
+                //Play sound
+                panel.playSound(memory.get(playbackPlace));
+
+                //Set lights
+                panel.clearLights();
+                panel.setLight(buttonToLight(memory.get(playbackPlace)), 1);
+
+                //Move to next sound
+                playbackPlace++;
+            }
+            else
+            {
+                playbackPlace = 0;
+                panel.clearLights();
+                panel.setLight(10, 2);
+                state = State.UserInput;
+            }
+        }
+
     }
 
     private int gridButtons(Panel panel)
@@ -46,6 +108,41 @@ public class Echo extends Minigame {
             }
         }
         return -1;
+    }
+
+    private String getRandomButton(List<String>list){ //choose a random button from the list
+        int index = random.nextInt(list.size());
+        return list.get(index);
+    }
+
+    private int buttonToLight(String s)
+    {
+        switch(s)
+        {
+            case "pause":
+                return 0;
+            case "mm0":
+                return 1;
+            case "mm1":
+                return 2;
+            case "mm2":
+                return 3;
+            case "mm3":
+                return 4;
+            case "mm4":
+                return 5;
+            case "mm5":
+                return 6;
+            case "mm6":
+                return 7;
+            case "mm7":
+                return 8;
+            case "mm8":
+                return 9;
+            case "mm9":
+                return 10;
+        }
+        return 0;
     }
 
 }
